8572a1368b7052deb538df202995a2cd6d20e892
NadiiaUa23 <hasiminka92@gmail.com>
2024-02-24 19:23:47 +0100

    готов к задачам

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,1 +1,11 @@
-console.log("Gallary");
+
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+
+const lightbox = new SimpleLightbox('.gallery a', {
+     /* options */ 
+    });
+ 
